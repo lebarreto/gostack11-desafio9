b8999e3795dc7322222968ed3a68a23fec06efb8
leticialmb <leticiabarreto196@gmail.com>
2020-05-15 16:05:40 -0400

    Desafio 9

--- src/modules/orders/services/CreateOrderService.ts
+++ src/modules/orders/services/CreateOrderService.ts
@@ -10,6 +10,7 @@ import IOrdersRepository from '../repositories/IOrdersRepository';
 interface IProduct {
   id: string;
   quantity: number;
+  name: string;
 }
 
 interface IRequest {
@@ -20,13 +21,32 @@ interface IRequest {
 @injectable()
 class CreateProductService {
   constructor(
+    @inject('OrdersRepository')
     private ordersRepository: IOrdersRepository,
+
+    @inject('ProductsRepository')
     private productsRepository: IProductsRepository,
+
+    @inject('CustomersRepository')
     private customersRepository: ICustomersRepository,
   ) {}
 
   public async execute({ customer_id, products }: IRequest): Promise<Order> {
-    // TODO
+    // const checkIfCustomerExists = await this.customersRepository.findById(
+    //   customer_id,
+    // );
+    // if (!checkIfCustomerExists) {
+    //   throw new AppError('This customer does not exists.', 400);
+    // }
+    // const checkIfProductExists = await this.productsRepository.findByName(
+    //   products[0].name,
+    // );
+    // if (!checkIfProductExists) {
+    //   throw new AppError('This product does not exists.', 400);
+    // }
+    // const product = await this.ordersRepository.create({
+    // });
+    // return product;
   }
 }
 
